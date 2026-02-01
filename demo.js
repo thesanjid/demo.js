b06f81a3fd1ede57c41f8206326bf177ed2add06
thenamesanjid <hsanjid07@gmail.com>
2026-02-02 02:11:17 +0600

    jsfanction dupate

--- demo.js
+++ demo.js
@@ -1,5 +1,38 @@
-let a= 1
+/*let a= 1
 let b=2 
-a=5
+a=5 
 
- console.log(a+b);
+ console.log(a+b);*/
+
+let x=5 
+let y=6
+let z=10 
+console.log(x-y+z)
+function hello (){
+    console.log("hello world") 
+
+}
+hello()
+
+function isUserLoggedIn(isLoggedIn) {
+  if (isLoggedIn) {
+    return "User is logged in";
+  } else {
+    return "Please log in first";
+  }
+}
+
+console.log(isUserLoggedIn(true));
+
+
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  }
+  return "Odd";
+}
+
+console.log(isEven(7)); // Odd
+
+
