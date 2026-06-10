044aca45e4ff1778775983e9755c576fe5784493
sanjid hasan <258803214+thesanjid@users.noreply.github.com>
2026-06-11 01:44:10 +0600

    Add file reading functionality to demo.js

--- demo.js
+++ demo.js
@@ -335,3 +335,10 @@ fs.readFile('file.txt', 'utf8', (err, data) => {
 });
 
 console.log('Done');
+
+const fs = require('fs');
+
+const data = fs.readFileSync('data.txt', 'utf8');
+console.log(data);
+
+
